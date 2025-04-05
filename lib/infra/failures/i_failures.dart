6eeec8afe58ee8fa46f305117fde7c2eb16e667c
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2025-04-04 22:53:30 -0300

    feat(Failure): Implement not found failure

--- lib/infra/failures/i_failures.dart
+++ lib/infra/failures/i_failures.dart
@@ -1,3 +1,9 @@
 abstract interface class IFailure {}
 
 class Empty implements IFailure {}
+
+class NotFoundFailure implements IFailure {
+  final String message;
+
+  const NotFoundFailure({this.message = ''});
+}
