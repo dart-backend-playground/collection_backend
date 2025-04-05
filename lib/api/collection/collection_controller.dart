557640d055695375f154e4ae404857694781f4aa
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2025-04-04 22:54:47 -0300

    feat(Collection Controller): Improved function return
    - Verify failure

--- lib/api/collection/collection_controller.dart
+++ lib/api/collection/collection_controller.dart
@@ -5,6 +5,7 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 import './../../services/generic_service.dart';
 import './../../api/api.dart';
+import '../../infra/failures/i_failures.dart';
 
 class CollectionController extends Api {
   final GenericService service;
@@ -14,8 +15,15 @@ class CollectionController extends Api {
   });
 
   FutureOr<Response> _findOne(Request req) async {
-    final result = await service.findOne(req.params['id'] ?? '');
-    return Response.ok(jsonEncode(result.toJson()));
+    final (result, failure) = await service.findOne(req.params['id'] ?? '');
+
+    if (failure is Empty) {
+      return Response.ok(jsonEncode(result.toJson()));
+    } else if (failure is NotFoundFailure) {
+      return Response.notFound(jsonEncode(failure.message));
+    }
+
+    return Response.forbidden(jsonEncode('Forbidden'));
   }
 
   @override
