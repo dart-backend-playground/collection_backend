791df91827a184ee2b81e65291fba2f91dd7fafc
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2025-05-13 13:09:19 -0300

    feat(Change class return): Change to use generic class

--- lib/infra/security/i_security_service.dart
+++ lib/infra/security/i_security_service.dart
@@ -2,7 +2,7 @@ import 'package:shelf/shelf.dart';
 
 abstract class ISecurityService<T, U> {
   Future<String> login(String email, password);
-  Future<String> logout();
+  Future<U> logout(String accessToken);
   Future<String> register(String email, password);
   Future<U> validateJWT(String token);
 
